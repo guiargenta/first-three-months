656a421a1a96c3167bf254d121780f2e6d44b8fd
guiargenta <161180165+guiargenta@users.noreply.github.com>
2024-04-03 11:08:33 -0300

    Imprima de 1000 a 0 usando WHILE.
    Utilizando while, crie um programa que imprime os números de 0 a 1000 em ordem decrescente (ou seja, de 1000 a 0).

---
+++ exercício10.py
@@ -0,0 +1,11 @@
+# Utilizando while, crie um programa que imprime os números de 0 a 1000 em ordem decrescente (ou seja, de 1000 a 0).
+
+x = 1000
+
+print("Vamos contar de 1000 a 0!")
+
+while x >= 0:
+    print("x =", x)
+    x = x - 1
+
+print("Programa Finalizado!")
